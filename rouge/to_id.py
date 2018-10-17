f7a1421fc41518ea5e9bc25daaeaae707d6c0bce
yueyongjiao <yueyongjiao@ruc.edu.cn>
2018-10-17 20:35:02 +0800

    add biaodian douhao

--- rouge/to_id.py
+++ rouge/to_id.py
@@ -19,7 +19,9 @@ fw = open(s + '_id', 'wb')
 for line in f:
     res = line.strip().split(' ')
     for i in range(len(res) - 1):
+        print(wd[res[i].decode('utf-8')])
         fw.write(str(wd[res[i].decode('utf-8')]) + ' ')
+
     fw.write(str(wd[res[-1]]) + '\n')
 fw.close()
 f.close()
