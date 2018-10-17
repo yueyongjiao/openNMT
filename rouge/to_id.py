3a72a43bb0ddd087c7bd738eb677c0720a863584
yueyongjiao <yueyongjiao@ruc.edu.cn>
2018-10-17 20:19:28 +0800

    add biaodian douhao

--- rouge/to_id.py
+++ rouge/to_id.py
@@ -1,7 +1,7 @@
 # coding:utf8
 import sys
 
-s = sys.argv[1]
+s = "/home/yueyongjiao/gitProject/python2_workplace/openNMT/openNMT/EventTeller/test_target.txt"
 wd = {}
 f = open('word_dic')
 wid = 0
@@ -11,10 +11,12 @@ for line in f:
     wid += 1
 f.close()
 wd['<unk>'] = wid
-print (len(wd))
+print(len(wd))
 wd[''] = 0
 
-f = open(s, 'r', 'utf8')
+# f = open(s, 'r', 'utf-8')
+
+f = open(s)
 fw = open(s + '_id', 'w')
 for line in f:
     res = line.strip().split(' ')
