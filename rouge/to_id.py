b42fc17ad08db2b2a185fd6a2991b0151f2d9b53
yueyongjiao <yueyongjiao@ruc.edu.cn>
2018-10-17 20:43:41 +0800

    add biaodian douhao

--- rouge/to_id.py
+++ rouge/to_id.py
@@ -23,7 +23,7 @@ wd['<unk>'] = wid
 print(len(wd))
 wd[''] = 0
 
-f = open(s,'r','utf-8')
+f = open(s, 'r', encoding='utf-8')
 fw = open(s + '_id', 'wb')
 for line in f:
     res = line.strip().split(' ')
